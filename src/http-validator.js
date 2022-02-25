ca129371e57cc37cd7ef62cd9b07b57e75ccd266
Diego <diegodvanelli@gmail.com>
2022-02-25 06:01:01 -0300

    Validate links

--- src/http-validator.js
+++ src/http-validator.js
@@ -1,5 +1,15 @@
+import fetch from "node-fetch";
+
+// function checkStats(arrayURLs) {
+//     const arryStats 
+// }
+
+function creatArrayURL(arrayLinks) {
+    return arrayLinks.map(objetctLink => objetctLink.map(link => Object.values(link).join()));
+}
+
 function checkURLs(arrayLinks) {
-    return arrayLinks
+    return creatArrayURL(arrayLinks);
 }
 
 export default checkURLs;
