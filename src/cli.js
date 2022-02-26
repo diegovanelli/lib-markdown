90ef719a9f52ee66c8114b017b13b62e746f87eb
Diego <diegodvanelli@gmail.com>
2022-02-26 11:29:43 -0300

    Create check urls and object with reponse

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ const path = process.argv;
 async function textProcess(filePath) {
     const result = await getFile(filePath[2]);
     if (path[3] === 'validar') {
-        console.log(chalk.yellow('validated links'), checkURLs(result))
+        console.log(chalk.yellow('validated links'), await checkURLs(result))
     } else {
         console.log(chalk.yellow('list of links'), result);
     }
--- src/http-validator.js
+++ src/http-validator.js
@@ -1,15 +1,49 @@
 import fetch from "node-fetch";
 
-// function checkStats(arrayURLs) {
-//     const arryStats 
-// }
-
-function creatArrayURL(arrayLinks) {
-    return arrayLinks.map(objetctLink => objetctLink.map(link => Object.values(link).join()));
+function handleError(error) {
+    throw new Error(error.message);
 }
 
-function checkURLs(arrayLinks) {
-    return creatArrayURL(arrayLinks);
+async function checkStats(arrayURLs) {
+    try {
+        const arrayStats = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                    const res = await fetch(url);
+                    return `${res.status} - ${res.statusText}`;
+                })
+            );
+        return arrayStats;
+    } catch(error) {
+        handleError(error);
+    }
+}
+
+function creatArrayURL(arrayLinks) {
+    const url = [];
+    arrayLinks
+        .map(objetctLink => objetctLink
+            .map(link => url.push(link)));
+    return url;
+}
+
+function creatURLs(arrayLinks) {
+    return arrayLinks
+        .map(objetctLink => Object
+            .values(objetctLink).join());
+}
+
+async function checkURLs(arrayLinks) {
+    const fileLinks = creatArrayURL(arrayLinks);
+    const links = creatURLs(fileLinks);
+    const statusLinks = await checkStats(links);
+
+    const results = fileLinks.map((object, index) => ({
+        ...object,
+        status: statusLinks[index]
+    }));
+
+    return results;
 }
 
 export default checkURLs;
